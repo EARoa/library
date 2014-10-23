c1e776a846babc3238a5a041f8e697421d482a68
Efrain Ayllon <jesusayllonroa@gmail.com>
2014-10-23 09:22:49 -0500

    Added seeds

--- library/db/seeds.rb
+++ library/db/seeds.rb
@@ -2,6 +2,11 @@
 Book.create title: "Catcher in the Rye", author: "J.D. Salinger", year: 1951
 Book.create title: "On the Road", author: "Jack Kerouac", year: 1956
 Book.create title: "Moby Dick", author: "Herman Melville", year: 1851
+Book.create title: "Shit's Broke", author: "Jesse Wolgomott", year: 2014
+Book.create title: "Fucking Learn Ruby", author: "Jesse Wolgomott", year: 2013
+Book.create title: "I don't know what I'm doing", author: "Efrain Ayllon", year: 2010
+
 
 Patron.create name: "Jesse Wolgomott"
 Patron.create name: "Chunda Bear"
+Patron.create name: "Efrain Ayllon"
